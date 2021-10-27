fee73c2e7f096e6fc6d2ae0ea20a00d8aa213e0e
Shabina Patel <79164944+ShabinaP@users.noreply.github.com>
2021-10-27 17:50:04 +0100

    file change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+// eslint-disable-next-line import/no-useless-path-segments
+import App from "../src/components/App";
 import "./styles/index.css";
 
 ReactDOM.render(
